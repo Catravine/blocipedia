cd3202e5d9ad2e56319f8cfe309c1fe5a0eef128
Caroline Courtney <caroline@carolinecourtney.com>
2016-03-23 01:51:26 -0400

    Seeding data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,46 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Create Users
+10.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: "helloworld",
+    password_confirmation: "helloworld",
+    confirmed_at: Date.today
+  )
+end
+
+# Create an admin user for my primary email
+caroline = User.create!(
+  email: "caroline@carolinecourtney.com",
+  password: "helloworld",
+  password_confirmation: "helloworld",
+  confirmed_at: Date.today
+)
+caroline.admin!
+
+# Create a premium user for my secondary email
+catravine = User.create!(
+  email: "catravine@hotmail.com",
+  password: "helloworld",
+  password_confirmation: "helloworld",
+  confirmed_at: Date.today
+)
+catravine.premium!
+
+# Total Users
+users = User.all
+
+# Create Wikis
+50.times do
+  Wiki.create(
+    title: Faker::Commerce.product_name,
+    body: Faker::Lorem.paragraphs(3),
+    public: [true, false].sample,
+    user: users.sample
+    )
+end
+wikis = Wiki.all
+
+# Results
+puts "Seed Finished..."
+puts "#{User.count} users created"
+puts "#{Wiki.count} wikis created"
