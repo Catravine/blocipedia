482de9618569a5058f6ad3ad136eea1e2b7202b8
Caroline Courtney <caroline@carolinecourtney.com>
2016-03-24 02:40:34 -0400

    Users can pay to upgrade

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,9 @@
+# Test -or- Production
+Rails.configuration.stripe = {
+  :publishable_key => ENV['stripe_test_publishable_key'],
+  #:publishable_key => ENV['stripe_live_publishable_key'],
+  :secret_key      => ENV['stripe_test_secret_key']
+  #:secret_key      => ENV['stripe_live_secret_key']
+}
+
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   devise_for :users
   #devise_for :users, :controllers => { :sessions => 'sessions' }
 
+  # Stripe payment ChargesController
+  resources :charges
+
   get 'about' => 'welcome#about'
   root 'welcome#index'
 
