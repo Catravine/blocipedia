4fa5ac2a398c3208cef52962e3a0556e8456b3a2
Caroline Courtney <caroline@carolinecourtney.com>
2016-03-30 03:12:03 -0400

    GoRails Stripe Subscription Ep 90 done

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -7,6 +7,24 @@ class SubscriptionsController < ApplicationController
   end
 
   def create
+    customer = if current_user.stripe_id?
+                  Stripe::Customer.retrieve(current_user.stripe_id)
+                else
+                  Stripe::Customer.create(email: current_user.email)
+                end
+    subscription = customer.subscriptions.create(
+      source: params[:stripeToken],
+      plan: "premium"
+    )
+    current_user.update(
+      stripe_id: customer.id,
+      stripe_subscription_id: subscription.id,
+      card_last4: params[:card_last4],
+      card_exp_month: params[:card_exp_month],
+      card_exp_year: params[:card_exp_year],
+      card_type: params[:card_brand]
+    )
+    redirect_to root_path
   end
 
   def destroy
