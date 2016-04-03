e5c7913108eaaa72cddd71d11be2939acb22ed73
Caroline Courtney <caroline@carolinecourtney.com>
2016-04-02 22:31:06 -0400

    Ajax collabs except for proper views...

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -1,27 +1,33 @@
 class CollaboratorsController < ApplicationController
 
   def create
-    user = User.find(params[:user_id])
+    @user = User.find(params[:user_id])
     @wiki = Wiki.find(params[:wiki_id])
-    collaborator = Collaborator.new(user: user, wiki: @wiki)
-    if collaborator.save
-      flash[:notice] = "Collaborator added."
+    @collaborator = Collaborator.new(user: @user, wiki: @wiki)
+    if @collaborator.save
+      flash[:notice] = "Collaborator '#{@user.name}' added."
     else
       flash[:alert] = "Collaboration failed."
     end
-    redirect_to edit_wiki_path(@wiki)
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def destroy
-    user = User.find(params[:user_id])
+    @user = User.find(params[:user_id])
     @wiki = Wiki.find(params[:wiki_id])
-    collaborator = Collaborator.find_by(user: user, wiki: @wiki)
-    if collaborator.destroy
+    @collaborator = Collaborator.find_by(user: @user, wiki: @wiki)
+    if @collaborator.destroy
       flash[:notice] = "Collaborator removed."
     else
       flash[:alert] = "De-Collaboration failed."
     end
-    redirect_to edit_wiki_path(@wiki)
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
 end
